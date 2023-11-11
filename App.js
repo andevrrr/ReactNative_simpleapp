e764d5c0a2289352a9fc1caf2e8a4b6f3096259c
Anton <kucherenko-anton1@mail.ru>
2023-11-11 23:58:05 +0200

    styling

--- App.js
+++ App.js
@@ -32,7 +32,7 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
-    padding: 21,
+    padding: 20,
     backgroundColor: '#fff',
   },
   input: {
